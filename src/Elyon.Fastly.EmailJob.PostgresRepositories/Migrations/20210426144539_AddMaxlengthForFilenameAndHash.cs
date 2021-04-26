4fd405dd170ed124281029e6fd4852d34bbb3342
Anatoli Angelov <anatoli.angelov@primeholding.com>
2021-04-26 17:49:26 +0300

    Add max lenght of file name and hash

---
+++ src/Elyon.Fastly.EmailJob.PostgresRepositories/Migrations/20210426144539_AddMaxlengthForFilenameAndHash.cs
@@ -0,0 +1,72 @@
+﻿#region Copyright
+// openLAMA is an open source platform which has been developed by the
+// Swiss Kanton Basel Landschaft, with the goal of automating and managing
+// large scale Covid testing programs or any other pandemic/viral infections.
+
+// Copyright(C) 2021 Kanton Basel Landschaft, Switzerland
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+// See LICENSE.md in the project root for license information.
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see https://www.gnu.org/licenses/.
+#endregion
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Elyon.Fastly.EmailJob.PostgresRepositories.Migrations
+{
+    public partial class AddMaxlengthForFilenameAndHash : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginalXXHash",
+                schema: "EmailJobSchema",
+                table: "Attachments",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileName",
+                schema: "EmailJobSchema",
+                table: "Attachments",
+                type: "character varying(200)",
+                maxLength: 200,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "OriginalXXHash",
+                schema: "EmailJobSchema",
+                table: "Attachments",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FileName",
+                schema: "EmailJobSchema",
+                table: "Attachments",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(200)",
+                oldMaxLength: 200);
+        }
+    }
+}
